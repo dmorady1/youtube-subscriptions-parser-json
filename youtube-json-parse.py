ab5eb9cff43fb12fc5119648bb650fa5aec65034
David Shahin Morady <moradydavid@web.de>
2020-11-28 18:29:59 +0100

    delete lazy option

--- youtube-json-parse.py
+++ youtube-json-parse.py
@@ -15,44 +15,27 @@ from chromedriver_py import binary_path
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--lazy',action='store_true')
-
+    parser.add_argument('--scroll', default='100', type=int)
 
     config = parser.parse_args()
 
-    email = None
-    password = None
-    if config.lazy:
-        email = input('Enter your email: ')
-        password = getpass('Enter your password: ')
 
     driver = webdriver.Chrome(executable_path=binary_path)
     # driver = webdriver.Chrome('./chromedriver') # local driver
 
 
-    if config.lazy:
-        url = "https://accounts.google.com/o/oauth2/auth/identifier?operation=login&state=google-%7Chttps%3A%2F%2Fmedium.com%2F%3Fsource%3Dlogin--------------------------lo_home_nav-----------%7Clogin&access_type=online&client_id=216296035834-k1k6qe060s2tp2a2jam4ljdcms00sttg.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fmedium.com%2Fm%2Fcallback%2Fgoogle&response_type=id_token%20token&scope=email%20openid%20profile&nonce=e3201beff5c040ce49e1db2009ebdc7db746a9b2f21b99e9f674f1a9151a328e&flowName=GeneralOAuthFlow"
-        driver.get(url)
-        sleep(5)
-        driver.find_element_by_css_selector("#identifierId").send_keys(f'{email}\n')
-
-        driver.implicitly_wait(1)
-        sleep(5)
-
-        driver.find_element_by_css_selector("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input").send_keys(f'{password}'+'\n')
-    if not config.lazy:
-        print('1. Go to https://medium.com/')
-        print('2. Click on Sign In')
-        print('3. Sign In with Google')
-        print('Even if the message "Sorry, we didn’t recognize that account." comes,\n you are logged in.')
-        print('Quick Check: If you can see your icon next to the url bar, then you are logged in\n')
-        print('----------')
-        input('Press enter if you are logged into google')
+    print('1. Go to https://medium.com/')
+    print('2. Click on Sign In')
+    print('3. Sign In with Google')
+    print('Even if the message "Sorry, we didn’t recognize that account." comes,\n you are logged in.')
+    print('Quick Check: If you can see your icon next to the url bar, then you are logged in\n')
+    print('----------')
+    input('Press enter if you are logged into google')
     driver.get('https://www.youtube.com/feed/channels?app=desktop')
     sleep(2)
     driver.refresh()
 
-    for i in range(100):
+    for i in range(config.scroll):
         html = driver.find_element_by_tag_name('html')
         html.send_keys(Keys.END)
 
@@ -80,10 +63,10 @@ def main():
 
     # print(all_names)
 
-    print(len(all_links))
+    print('Number of channel links: ', len(all_links))
 
-    print(len(all_names))
-    print(len(all_names) == len(all_links))
+    print('Number of channel names: ', len(all_names))
+    print('Are the number of links and names equal?: ', len(all_names) == len(all_links))
 
     if len(all_names) == 0 or len(all_links) == 0:
         print('Please try again. Sometimes there are issue with the youtube page.')
